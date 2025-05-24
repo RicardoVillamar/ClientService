506d6588ef209b5efce285d66a1088c5bf35ca18
Ricardo <ricardo.villamarmin@ug.edu.ec>
2025-05-24 02:29:21 -0500

    add: service-registers module complete design

--- lib/view/servicio/registro_alquiler.dart
+++ lib/view/servicio/registro_alquiler.dart
@@ -1,7 +1,10 @@
 import 'package:client_service/utils/colors.dart';
 import 'package:client_service/utils/font.dart';
+import 'package:client_service/view/widgets/shared/button.dart';
 import 'package:client_service/view/widgets/shared/inputs.dart';
+import 'package:client_service/view/widgets/shared/toolbar.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class RegistroAlquiler extends StatefulWidget {
   const RegistroAlquiler({super.key});
@@ -15,6 +18,50 @@ class _RegistroAlquilerState extends State<RegistroAlquiler> {
   final TextEditingController _direccion = TextEditingController();
   final TextEditingController _telefono = TextEditingController();
   final TextEditingController _correo = TextEditingController();
+
+  // Date picker
+  final TextEditingController _dateController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      final formattedDate = DateFormat('dd/MM/yyyy').format(picked);
+      setState(() {
+        _dateController.text = formattedDate;
+      });
+    }
+  }
+
+  // Date picker
+  final TextEditingController _dateControllerTrabajo = TextEditingController();
+
+  Future<void> _selectDateTrabajo(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      final formattedDate = DateFormat('dd/MM/yyyy').format(picked);
+      setState(() {
+        _dateController.text = formattedDate;
+      });
+    }
+  }
+
+  String? selectValue;
+  List<String> personal = [
+    'Tipo 1',
+    'Tipo 2',
+    'Tipo 3',
+    'Tipo 4',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -68,25 +115,25 @@ class _RegistroAlquilerState extends State<RegistroAlquiler> {
                     child: Column(
                       children: [
                         TxtFields(
-                          label: 'Nombre comercial',
+                          label: 'Nombre comercial*',
                           controller: _nombreC,
                           screenWidth: screenWidth,
                           showCounter: false,
                         ),
                         TxtFields(
-                          label: 'Direccion',
+                          label: 'Direccion*',
                           controller: _direccion,
                           screenWidth: screenWidth,
                           showCounter: false,
                         ),
                         TxtFields(
-                          label: 'Telefono',
+                          label: 'Telefono*',
                           controller: _telefono,
                           screenWidth: screenWidth,
                           showCounter: false,
                         ),
                         TxtFields(
-                          label: 'Correo electronico',
+                          label: 'Correo electronico*',
                           controller: _correo,
                           screenWidth: screenWidth,
                           showCounter: false,
@@ -108,12 +155,90 @@ class _RegistroAlquilerState extends State<RegistroAlquiler> {
                               ),
                               const SizedBox(height: 10),
                               const Row(
-                                children: [],
+                                children: [
+                                  // radio buttons
+
+                                  Radio(
+                                    value: 1,
+                                    groupValue: 0,
+                                    onChanged: null,
+                                  ),
+                                  Text('Tipo 1'),
+
+                                  SizedBox(width: 20),
+                                  Radio(
+                                    value: 2,
+                                    groupValue: 0,
+                                    onChanged: null,
+                                  ),
+
+                                  Text('Tipo 2'),
+
+                                  SizedBox(width: 20),
+
+                                  Radio(
+                                    value: 3,
+                                    groupValue: 0,
+                                    onChanged: null,
+                                  ),
+
+                                  Text('Tipo 3'),
+                                ],
                               )
                             ],
                           ),
                         ),
                         const SizedBox(height: 20),
+                        TextFormField(
+                          controller: _dateController,
+                          readOnly: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Fecha de reserva',
+                            suffixIcon: Icon(Icons.calendar_today),
+                            border: OutlineInputBorder(),
+                          ),
+                          onTap: () => _selectDate(context),
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          controller: _dateControllerTrabajo,
+                          readOnly: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Fecha de trabajo',
+                            suffixIcon: Icon(Icons.calendar_today),
+                            border: OutlineInputBorder(),
+                          ),
+                          onTap: () => _selectDateTrabajo(context),
+                        ),
+                        const SizedBox(height: 20),
+                        TxtFields(
+                          label: 'Monto alquiler*',
+                          controller: _nombreC,
+                          screenWidth: screenWidth,
+                          showCounter: false,
+                        ),
+                        const SizedBox(height: 20),
+                        DropdownButton(
+                          isExpanded: true,
+                          hint: Text(
+                            'Personal que asistió*',
+                            style: AppFonts.inputtext,
+                          ),
+                          value: selectValue,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectValue = newValue;
+                            });
+                          },
+                          items: personal.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                        const SizedBox(height: 20),
+                        BtnElevated(text: 'Registro', onPressed: () {})
                       ],
                     ),
                   ),
@@ -123,6 +248,7 @@ class _RegistroAlquilerState extends State<RegistroAlquiler> {
           ],
         ),
       ),
+      bottomNavigationBar: const Toolbar(),
     );
   }
 }
