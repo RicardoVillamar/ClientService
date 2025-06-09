30886636977f33cd1389581f78bdca7d46fd393c
Savecoders <pablopincay2003@gmail.com>
2025-06-09 08:51:41 -0500

    feat: load environment variables and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:client_service/app.dart';
 import 'package:client_service/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main(List<String> args) async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter is initialized
+  try {
+    await dotenv.load(fileName: ".env"); // Load environment variables
+  } catch (e) {
+    throw Exception('Error loading .env file: $e'); // Print error if any
+  }
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
